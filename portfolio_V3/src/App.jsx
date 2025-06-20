238138dbe5b59f97bde17155fa0c3a00bb86c38f
Dope-Otaku <fakeyt909@gmail.com>
2025-06-21 01:12:02 +0530

    added navbar css and debugged it here

--- portfolio_V3/src/App.jsx
+++ portfolio_V3/src/App.jsx
@@ -1,5 +1,12 @@
 import Navbar from "./components/navbar/Navbar"
 import { Link } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
+// import Home from './components/Home'
+// import About from './components/About'
+// import Projects from './components/Projects'
+// import Resume from './components/Resume'
+// import Jewelry from './components/Jewelry'
+// import Contact from './components/Contact'
 
 
 function App() {
--- portfolio_V3/src/components/navbar/Navbar.jsx
+++ portfolio_V3/src/components/navbar/Navbar.jsx
@@ -1,15 +1,33 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './Navbar.css'
 
-function Navbar() {
-    let NavbarRoutes = [
+let NavbarRoutes = [
     {name: 'About', link:'/about'},
     {name: 'Projects', link:'/projects'},
     {name: 'Resume', link:'/resume'},
     {name: 'Home', link:'/home'},
     {name: 'Contact', link:'/contact'},
  ]
+
+
+function Navbar() {
+    
   return (
-    <div>Navbar</div>
+    <>
+    <div className="outerDiv">
+        <ul>
+            {
+            NavbarRoutes.map(link => (
+                <li key={link.name} className="navbar_routes">
+                <Link to={link.link}>{link.name}</Link>
+                </li>
+            ))
+            }
+        </ul>
+    </div>
+    
+    </>
   )
 }
 
