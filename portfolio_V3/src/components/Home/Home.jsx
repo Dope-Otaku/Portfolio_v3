229fa84bf0cf96c249e5646e796c057669c35889
Dope-Otaku <fakeyt909@gmail.com>
2025-06-21 01:48:25 +0530

    our main section is ready demo version

--- portfolio_V3/src/components/Home/Home.jsx
+++ portfolio_V3/src/components/Home/Home.jsx
@@ -5,7 +5,12 @@ function Home() {
   return (
     <>
         <div className="outerBox">
-            Body metadat comes here!
+            <div className="imageMeta"></div>
+            <div className="summaryMeta">
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis iste impedit veritatis at, suscipit non est, tenetur itaque soluta cum fugiat illum id nesciunt nemo dolor aliquam eaque quis culpa voluptate adipisci? Nihil blanditiis rem veritatis odio corrupti illum quo ab aliquid non eveniet, totam repellat modi soluta enim quidem, voluptatum libero optio numquam. Repellat voluptatem possimus nostrum mollitia impedit? Aliquam repudiandae id eum magnam vel voluptatum commodi inventore ex porro laudantium incidunt excepturi explicabo voluptatem doloribus soluta esse adipisci consequuntur, nulla exercitationem rerum. Quia quod, quis similique maxime adipisci est, nemo veritatis dolor excepturi porro vitae consectetur accusamus distinctio.
+                </p>
+            </div>
         </div>
     </>
   )
