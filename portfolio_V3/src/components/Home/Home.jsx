f59221ff7cff9c7b7bd787cc1158c5132b43ef56
Dope-Otaku <fakeyt909@gmail.com>
2025-06-21 03:06:26 +0530

    finaly changed to cors and implemented it

--- portfolio_V3/src/components/Home/Home.jsx
+++ portfolio_V3/src/components/Home/Home.jsx
@@ -1,15 +1,23 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
+import axios from "axios"
 
 import './Home.css'
 
 function Home() {
   const [text, setText] = useState("");
+
+  function getQuotes(){
+    axios.get("http://localhost:8080/",  { crossdomain: true }).then(response => {
+      setText(response.data.text);
+      // setAuthor(response.data.author);
+    });
+  }
   return (
     <>
         <div className="outerBox">
             <div className="imageMeta"></div>
             <div className="summaryMeta">
-              <button>
+              <button onClick={getQuotes}>
                 Generate Quote
               </button>
                 <p>
