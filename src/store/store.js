ed37b9d4b22e5f5a0a9c41af674aa4b643d732ea
Dope-Otaku <fakeyt909@gmail.com>
2025-07-13 01:13:43 +0530

    we have re-written store policy

---
+++ src/store/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+
+const mainStore = configureStore({
+    // we will add all the features directly here
+    reducer: {}
+})
+
+export default mainStore
