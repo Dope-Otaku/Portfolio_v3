526ed64ef5d276909892d9cb65bb2c2ca717a1eb
Dope-Otaku <fakeyt909@gmail.com>
2025-07-13 01:36:43 +0530

    we have connected all the feature section to mainStore

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
+import {reducer as authFeature} from "../features/authSlicer";
+import {reducer as postFeature} from "../features/postSlicer";
+import {reducer as projectFeature} from "../features/projectSlicer";
 
 
 const mainStore = configureStore({
     // we will add all the features directly here
-    reducer: {}
+    reducer: {
+        auth: authFeature,
+        postAuth: postFeature,
+        projectAuth: projectFeature
+    }
 })
 
 export default mainStore
