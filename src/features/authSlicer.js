12659620e6e9888f2b522317a82de3db36331622
Dope-Otaku <fakeyt909@gmail.com>
2025-07-13 01:20:39 +0530

    we have written empty logics for authentication feature

---
+++ src/features/authSlicer.js
@@ -0,0 +1,24 @@
+/*
+in this file we will mainly expose controls for login, logout mechanism and not of register!
+*/
+
+import { createSlice } from "@reduxjs/toolkit"
+
+const authInitialState = {
+    status: false, //id current status whether active or not
+    userData : null //other data such as username, password, sessionID, etc. we don't need to add everything here we can just destructure it here.
+}
+
+
+const authSlicer = createSlice({
+    name: "auth",
+    initialState: authInitialState,
+    reducers:{
+        login:()=>{},
+        logout:()=>{},
+        fgPassword:()=>{}
+    }
+})
+
+export const {login, logout, fgPassword} = authSlicer.actions
+export default authSlicer.reducer
