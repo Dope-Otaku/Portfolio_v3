eedd5d5c04fe0a783bb48c429ff264a69df6bc9e
Dope-Otaku <fakeyt909@gmail.com>
2025-07-13 11:34:55 +0530

    just testing out the conf file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import conf from "./creds/conf"
 
 
 function App() {
+  // const noi = () => {
+  //   conf.appWriteBucketID
+  // }
 
   return (
     <>
-      <h1 className="text-center text-3xl bg-green-500 text-white">Hello world!</h1>
+      <h1 className="text-center text-3xl bg-green-500 text-white">{conf.appWriteBucketID || `Hello world!`}</h1>
     </>
   )
 }
