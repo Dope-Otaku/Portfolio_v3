6e213c5131a257c5ab8cc0085b1a43e6c442ed91
Dope-Otaku <fakeyt909@gmail.com>
2025-07-14 22:05:28 +0530

    added reverseproxy endpoint in cred

--- src/creds/conf.js
+++ src/creds/conf.js
@@ -3,7 +3,8 @@ const conf = {
     appWriteProjectID: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
     appWriteDatabaseID: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appWriteCollectionID: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-    appWriteBucketID: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+    appWriteBucketID: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+    appWritezreverseProxyId: String(import.meta.env.VITE_APPWRITE_RECOVERY_ENDPOINT)
 }
 
 export default conf;
