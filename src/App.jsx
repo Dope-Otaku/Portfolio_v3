7a70bd52a23367281a4223fd53aebb393bd3565c
Dope-Otaku <fakeyt909@gmail.com>
2025-07-16 23:24:19 +0530

    adding new comments of what to do

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,9 @@ function App() {
   
   return (
     <>
+      {/* ok so now we are going to change our home page and addding all these functions besides
+      this because the only thing we needed was auth which took time but it did and now we need to
+      add post also */}
       {/* <Login/> */}
       <ForgotPassword/>
       <Outlet/>
