ea74b83f0d82621a92e737da6cdef5f7771355f0
Dope-Otaku <fakeyt909@gmail.com>
2025-07-17 23:33:06 +0530

    first bug debugging failed

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import Login from "./Login"
 import ForgotPassword from "./ForgotPassword"
 import ForgotPasswordRecovery from "./ForgotPasswordRecovery"
 import { Outlet } from "react-router-dom"
+import useMouseHover from "./UseMouseHover"
 
 
 
 function App() {
+  const {x, y} = useMouseHover()
   
   
   return (
@@ -14,8 +16,12 @@ function App() {
       this because the only thing we needed was auth which took time but it did and now we need to
       add post also */}
       {/* <Login/> */}
+      <div>
+        {x !== null && y !== null ? `Your cursor is at ${x}, ${y}.` : "Move your mouse around."}
+      </div>
 
       {/* another day missed but we are still going to implement one thing */}
+      
       <ForgotPassword/>
       <Outlet/>
     </>
---
+++ src/UseMouseHover.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react'
+
+function useMouseHover() {
+    const [mouseMover, setMouseMover] = useState({
+        x: null, y: null
+    })
+
+    useEffect(() => {
+        const updatePointer = (event) => {
+            setMouseMover({ x: event.clientX, y: event.clientY })
+        }
+        window.addEventListener('mousemove', updatePointer)
+
+        return () => {
+            window.removeEventListener('mousemove', updatePointer)
+        }
+    }, [])
+
+    return mouseMover
+}
+
+export default useMouseHover
