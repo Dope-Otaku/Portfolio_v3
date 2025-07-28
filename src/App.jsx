e4fa3be070d5bd80f126cc7f44cc6d887a18c91d
Dope-Otaku <fakeyt909@gmail.com>
2025-07-28 22:43:40 +0530

    completed the upfront image progress 1

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Login from "./Login"
 import ForgotPassword from "./ForgotPassword"
 import ForgotPasswordRecovery from "./ForgotPasswordRecovery"
 import { Outlet } from "react-router-dom"
+import upfront from "../src/assets/upfront.png"
 
 
 
@@ -10,12 +11,22 @@ function App() {
   
   return (
     <>
-      <div className="flex justify-center items-center min-h-screen gap-8 bg-gradient-to-br from-gray-100 to-gray-300">
-        <div className="w-1/5 h-90 bg-red-500/20 backdrop-blur-lg border border-white/30 rounded-2xl shadow-xl flex items-center justify-center text-red-600 font-bold text-xl hover:bg-red-500/30 transition-all duration-300">
-          1
+      <div 
+        className="flex justify-center items-center min-h-screen gap-8 bg-no-repeat"
+        style={{
+          backgroundImage: `url(${upfront})`,
+          backgroundBlendMode: 'overlay',
+          backgroundSize: '60% auto', 
+          backgroundPosition: 'center 10%' 
+        }}
+      >
+        <div className="absolute inset-0 bg-black/10"></div>
+        
+        <div className="relative z-10 w-1/5 h-60 bg-red-500/20 cursor-pointer backdrop-blur-lg border border-white/30 rounded-2xl shadow-xl flex items-center justify-center text-red-600 font-bold text-xl hover:bg-red-500/30 transition-all duration-300">
+          Recruiter
         </div>
-        <div className="w-1/5 h-90 bg-blue-500/20 backdrop-blur-lg border border-white/30 rounded-2xl shadow-xl flex items-center justify-center text-blue-600 font-bold text-xl hover:bg-blue-500/30 transition-all duration-300">
-          2
+        <div className="relative z-10 w-1/5 h-60 bg-blue-500/20 cursor-pointer backdrop-blur-lg border border-white/30 rounded-2xl shadow-xl flex items-center justify-center text-blue-600 font-bold text-xl hover:bg-blue-500/30 transition-all duration-300">
+          Developer
         </div>
       </div>
       {/* <ForgotPassword />
