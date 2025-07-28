b7ad96ebd7e0bbff89cf12bc315e2655a2de192d
Dope-Otaku <fakeyt909@gmail.com>
2025-07-28 21:14:34 +0530

    added the base configuration for the nre upfrontpage

--- src/UpfrontPage.jsx
+++ src/UpfrontPage.jsx
@@ -1,3 +1,12 @@
 // this page is the first page the user sees after going into my website
 
 
+import React from 'react'
+
+function UpfrontPage() {
+  return (
+    <div>UpfrontPage</div>
+  )
+}
+
+export default UpfrontPage
