65b0de1cf98317bdc405b87bcc6e8bc4eaaac8ad
Dope-Otaku <fakeyt909@gmail.com>
2025-08-20 11:00:02 +0530

    trying to add a basic sketch

--- src/Developer.jsx
+++ src/Developer.jsx
@@ -5,6 +5,9 @@ function Developer() {
     <>
         <h1>Developer</h1>
         <span>need to add developer like effect in these</span>
+        <div>Developer section name</div>
+        <div>terminal</div>
+        <div>id card</div>
     </>
   )
 }
