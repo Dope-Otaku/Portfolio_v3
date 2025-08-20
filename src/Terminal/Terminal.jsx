7186707bb330e86ce2005882237dbdf484501f73
Dope-Otaku <fakeyt909@gmail.com>
2025-08-20 12:05:01 +0530

    we have created handle command function for terminal

--- src/Terminal/Terminal.jsx
+++ src/Terminal/Terminal.jsx
@@ -3,8 +3,39 @@ import React, {useState} from 'react'
 function Terminal() {
     const [history, setHistory] = useState([])
     const [currentInput, setCurrentInput] = useState("")
+
+    const handleCommand = (command) => {
+        let output = ""
+
+        switch (command.trim()) {
+            case "help":
+                output: "Welcome to Help section!"
+                break;
+
+            case "whoami":
+                output: "I am the king of laziness!"
+                break;
+
+            case "delete":
+                output: "you better watch it out Bitch!"
+                break;
+            case "clear":
+                setHistory([])
+                return;
+        
+            default:
+                if (command.startsWith("echo ")) {
+                        output = command.replace("echo ", "");
+                }
+                else {
+                    output = `Command not found: ${command}`;
+                }
+        }
+    }
+
+
   return (
-    <></>
+    <>Terminal Loading...</>
   )
 }
 
