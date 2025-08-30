42f0d5a52d850d9e34e57bf87334449ccbb8ce5d
Dope-Otaku <fakeyt909@gmail.com>
2025-08-30 21:13:31 +0530

    unable to complete this on deadline

--- src/components/Recruiter/Recruiter.jsx
+++ src/components/Recruiter/Recruiter.jsx
@@ -6,6 +6,7 @@ function Recruiter() {
         <h1>What should i add here?</h1>
         <span>but when?</span>
         <h3>I guess we need to add some cool design with my experience and projects!</h3>
+        <h3>deadline finished and still this is not complete!</h3>
     </>
   )
 }
